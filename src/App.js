42ffc872b72fefe0b37d8aee85d36305716d5708
dolores-kr <parangyro@gmail.com>
2021-02-18 05:47:07 +0000

    sub1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          created by dolores-kr.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
